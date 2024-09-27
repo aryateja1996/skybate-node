2a4c46659dc6f5a3a49a35f20712e82c3c51970d
sreekar1996 <sreekarkondaveeti76@gmail.com>
2024-09-27 09:43:44 +0530

    formated the files

--- consts/phonepe.js
+++ consts/phonepe.js
@@ -1,17 +1,17 @@
-const MERCHANT_ID = "PGTESTPAYUAT86";
-const SALT_KEY = "96434309-7796-489d-8924-ab56988a6076";
-const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
-const SALT_INDEX = 1;
-const APP_URL = "http://localhost:4200"
+// const MERCHANT_ID = "PGTESTPAYUAT86";
+// const SALT_KEY = "96434309-7796-489d-8924-ab56988a6076";
+// const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
+// const SALT_INDEX = 1;
+// const APP_URL = "http://localhost:4200"
 
 
 // Prod Creds
 
-// const MERCHANT_ID = "M22WMPBLHWK1W";
-// const SALT_KEY = "4bcd2400-6699-4052-8d8c-570bb5cb6f2d";
-// const PHONE_PE_HOST_URL = "https://api.phonepe.com/apis/hermes";
-// const SALT_INDEX = 1;
-// const APP_URL = "https://skybate.in/"
+const MERCHANT_ID = "M22WMPBLHWK1W";
+const SALT_KEY = "4bcd2400-6699-4052-8d8c-570bb5cb6f2d";
+const PHONE_PE_HOST_URL = "https://api.phonepe.com/apis/hermes";
+const SALT_INDEX = 1;
+const APP_URL = "https://skybate.in/"
 
 
-module.exports = {MERCHANT_ID,SALT_INDEX,SALT_KEY,PHONE_PE_HOST_URL,APP_URL};
+module.exports = { MERCHANT_ID, SALT_INDEX, SALT_KEY, PHONE_PE_HOST_URL, APP_URL };
